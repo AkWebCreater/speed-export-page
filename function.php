8057ca74d22db3633c2670e70b32444d03b0a3e0
Akash Sager <ak.web.creater@gmail.com>
2021-04-05 18:46:33 +0530

    added files for mobile sppeed

--- function.php
+++ function.php
@@ -4,7 +4,7 @@ $conn = getdb();
 // setting defult timezone to india timezone 
 date_default_timezone_set('Asia/Kolkata');
 
-$filename='uploded/speed_';
+$filename='uploded/desktop/speed_';
 $date = date('m-d-Y_h-i-s', time());
 $csv = $filename . $date .'.csv';
 echo $csv;
@@ -27,4 +27,28 @@ fputcsv($file, $row);
 }
 // closing csv file
 fclose($file);
+
+$filenamemob='uploded/mobile/speed_mobile_';
+$datemob = date('m-d-Y_h-i-s', time());
+$csv = $filenamemob . $datemob .'.csv';
+echo $csv;
+// new CSV file oppned
+$fileMob = fopen($csv, 'w');
+// colums header  of CSV file
+$colums = array('First Contentful Paint','First Input Delay','First Contentful Paint1','Speed Index','Time To Interactive','First Meaningful Paint','First Contentful Paint','First CPU Idle','Latency');
+fputcsv($fileMob,$colums);
+// sql database query
+$q = 'select * from speed_mobile';
+
+// code
+
+// putting content in  csv file 
+if($rows = mysqli_query($conn, $q)){
+while( $row = mysqli_fetch_assoc($rows))
+{
+fputcsv($fileMob, $row);
+}
+}
+// closing csv file
+fclose($fileMob);
 ?>
--- index.php
+++ index.php
@@ -54,4 +54,56 @@ else
 {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
+// mob curl
+$urlMob = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://lakmeindia.com/&strategy=MOBILE";
+echo $urlMob;
+$chMob = curl_init($urlMob);
+curl_setopt($chMob, CURLOPT_RETURNTRANSFER, true);
+$curl_scraped_page_mob = curl_exec($chMob);
+curl_close($ch);
+
+$speedMob = json_decode($curl_scraped_page_mob, true);
+
+$FirstContentfulPaintMob = $speedMob['loadingExperience']['metrics']['FIRST_CONTENTFUL_PAINT_MS']['category'];
+
+$FirstInputDelayMob = $speedMob['loadingExperience']['metrics']['FIRST_INPUT_DELAY_MS']['category'];
+
+$FirstContentfulPaint1Mob = $speedMob['lighthouseResult']['audits']['first-contentful-paint']['displayValue'];
+
+$SpeedIndexMob = $speedMob['lighthouseResult']['audits']['speed-index']['displayValue'];
+
+$TimeToInteractiveMob = $speedMob['lighthouseResult']['audits']['interactive']['displayValue'];
+
+$FirstMeaningfulPaintMob = $speedMob['lighthouseResult']['audits']['first-meaningful-paint']['displayValue'];
+
+$FirstCPUIdleMob = $speedMob['lighthouseResult']['audits']['first-meaningful-paint']['displayValue'];
+
+$latencyMob = $speedMob['lighthouseResult']['audits']['estimated-input-latency']['displayValue'];
+
+
+
+
+
+echo $timezon;
+echo $FirstContentfulPaintMob . "<br>";
+echo $FirstInputDelayMob . "<br>";
+echo $FirstContentfulPaint1Mob . "<br>";
+echo $SpeedIndexMob . "<br>";
+echo $TimeToInteractiveMob . "<br>";
+echo $FirstMeaningfulPaintMob . "<br>";
+echo $FirstCPUIdleMob . "<br>";
+echo $latencyMob . "<br>";
+
+$sql1 = "INSERT INTO `speed_mobile`(`First Contentful Paint Chrome`, `First Input Delay`, `First Contentful Paint lighthouse`, `Speed Index`, `Time To Interactive`, `First Meaningful Paint`, `First CPU Idle`, `Estimated Input Latency`) VALUES ('$FirstContentfulPaintMob','$FirstInputDelayMob','$FirstContentfulPaint1Mob','$SpeedIndexMob','$TimeToInteractiveMob','$FirstMeaningfulPaintMob','$FirstCPUIdleMob','$latencyMob')";
+
+if ($conn->query($sql1) === true)
+{
+    echo "New record created successfully in mobile";
+}
+else
+{
+    echo "Error: " . $sql1 . "<br>" . $conn->error;
+}
 ?>
